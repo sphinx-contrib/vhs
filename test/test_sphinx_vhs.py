887a4965bd538fd3cc6141c9ab28e3dba479286a
Tamika Nomara <taminomara@gmail.com>
2024-11-18 22:59:40 +0400

    Codestyle

--- test/test_sphinx_vhs.py
+++ test/test_sphinx_vhs.py
@@ -45,7 +45,9 @@ def patched_generate_vhs_worker_api():
 @pytest.mark.sphinx("html", testroot="basics")
 def test_app(app: util.SphinxTestApp, patched_generate_vhs_worker_api):
     (
-        get_generate_vhs_worker_runs, get_generate_vhs_compilation_runs, reset
+        get_generate_vhs_worker_runs,
+        get_generate_vhs_compilation_runs,
+        reset,
     ) = patched_generate_vhs_worker_api
 
     app.build()
@@ -62,17 +64,17 @@ def test_app(app: util.SphinxTestApp, patched_generate_vhs_worker_api):
     a_tape_gifs = etree.findall(".//img[@alt='a.tape']")
     assert len(a_tape_gifs) == 3
     a_tape_srcs = {e.attrib["src"] for e in a_tape_gifs}
-    assert a_tape_srcs == {'_images/vhs-a.gif', '_images/vhs-inline.gif'}
+    assert a_tape_srcs == {"_images/vhs-a.gif", "_images/vhs-inline.gif"}
 
     b_tape_gifs = etree.findall(".//img[@alt='b.tape']")
     assert len(b_tape_gifs) == 3
     b_tape_srcs = {e.attrib["src"] for e in b_tape_gifs}
-    assert b_tape_srcs == {'_images/vhs-b.gif', '_images/vhs-inline1.gif'}
+    assert b_tape_srcs == {"_images/vhs-b.gif", "_images/vhs-inline1.gif"}
 
     c_tape_gifs = etree.findall(".//img[@alt='c.tape']")
     assert len(c_tape_gifs) == 1
     c_tape_srcs = {e.attrib["src"] for e in c_tape_gifs}
-    assert c_tape_srcs == {'_images/vhs-inline2.gif'}
+    assert c_tape_srcs == {"_images/vhs-inline2.gif"}
 
     reset()
     app.build()
